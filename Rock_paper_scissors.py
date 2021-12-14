9e33e232efb5a88136d7aa289c0069f38574326b
Aravind <92704228+Lochanmadhurakavi@users.noreply.github.com>
2021-12-14 20:27:54 +0530

    Update Rock_paper_scissors.py

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -35,13 +35,13 @@ computer_choice = randint(0, 2)
 print(images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number")
+    print("You typed an invalid number.")
 
 elif computer_choice == user_choice:
     print("It's a tie.")
 
 elif (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2) or (user_choice == 2 and computer_choice == 0):
-    print("You lose")
+    print("You lose.")
 
 elif (user_choice == 0 and computer_choice ==1) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
     print("You win!")
